0930688be0d903229c5303050c40c6601ca56c53
Pasang-lama <lpasang182@gmail.com>
2024-06-30 09:20:46 +0545

    banner data

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
+import Banneritems from "./component/Banneritems"
 import Footer from "./component/Footer"
 import Header from "./component/Header"
-import About from "./pages/About"
-
 function App() {
 
   return (
     <>
       <Header />
-      <div className="container text-center my-5 py-5">
-        <h1>    body content goes here.....</h1>
-      </div>
+      <section className="banner">
+        <div id="carouselExampleCaptions" className="carousel slide">
+          <div className="carousel-inner">
+            <Banneritems />
+          </div>
+          <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true" />
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true" />
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
+      </section>
       <Footer />
     </>
   )
